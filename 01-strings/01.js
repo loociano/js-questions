f5a57af01deb6d0e3d6a8b9ccd830906470e9885
Luciano Rubio <luciano@loociano.com>
2015-09-04 09:16:09 +0100

    Second solution for 1.1 using bitwise operations

--- 01-strings/01.js
+++ 01-strings/01.js
@@ -20,6 +20,11 @@
  * f(1) // false (invalid input)
  * f(NaN) // false (invalid input)
  */
+
+/** 
+ * @param {string}
+ * @return {boolean}
+ */ 
 function hasAllUniqueCharacters(string){
 
   var alphabet = 'abcdefghijklmnopqrstuvwxyz'; // assumption
@@ -41,6 +46,7 @@ function hasAllUniqueCharacters(string){
 }
 
 // Tests
+console.log("Testing hasAllUniqueCharacters(string)");
 console.assert(hasAllUniqueCharacters('abc') === true, "FAILED: abc has unique characters");
 console.assert(hasAllUniqueCharacters('aabbcc') === false, "FAILED: aabbcc has duplicated characters");
 console.assert(hasAllUniqueCharacters('aaa') === false, "FAILED: aaa has duplicated characters");
@@ -49,3 +55,43 @@ console.assert(hasAllUniqueCharacters('') === false, "FAILED: string is empty");
 console.assert(hasAllUniqueCharacters('my name') === false, "FAILED: string contains invalid characters");
 console.assert(hasAllUniqueCharacters(1) === false, "FAILED: input is not a string");
 console.assert(hasAllUniqueCharacters(NaN) === false, "FAILED: input is NaN");
+
+
+/**
+ * Second solution
+ * Suggested by the book, with a bit checker
+ *
+ * It is not possible to implement exactly the same 256 bit checker in JavaScript,
+ * as JavaScript performs bitwise comparison using signed 32-bit integers.
+ * We will need to use 256 / 32 = 8 integers.
+ */
+function hasAllUniqueCharacters2(string){
+  var bitArray = [0, 0, 0, 0, 0, 0, 0, 0]; // 32x8 = 256 bits
+
+  if (!string || typeof string !== 'string'|| string.length > 256) {
+    return false;
+  }
+  
+  for (var i = 0, l = string.length; i < l; i++){
+    var charCode = string.charCodeAt(i);
+
+    var slot = charCode % 32;
+
+    if (bitArray[slot] & (1 << charCode) > 0){
+        return false;
+    }
+    bitArray[slot] |= (1 << charCode);
+  }
+  return true;
+}
+
+// Tests
+console.log("Testing hasAllUniqueCharacters2(string)");
+console.assert(hasAllUniqueCharacters2('abc') === true, "FAILED: abc has unique characters");
+console.assert(hasAllUniqueCharacters2('aabbcc') === false, "FAILED: aabbcc has duplicated characters");
+console.assert(hasAllUniqueCharacters2('aaa') === false, "FAILED: aaa has duplicated characters");
+console.assert(hasAllUniqueCharacters2(null) === false, "FAILED: string is null");
+console.assert(hasAllUniqueCharacters2('') === false, "FAILED: string is empty");
+console.assert(hasAllUniqueCharacters2('my name') === false, "FAILED: string contains invalid characters");
+console.assert(hasAllUniqueCharacters2(1) === false, "FAILED: input is not a string");
+console.assert(hasAllUniqueCharacters2(NaN) === false, "FAILED: input is NaN");
